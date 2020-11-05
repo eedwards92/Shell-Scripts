e889d615fa5d232cbaf39b0db7cf4e7dc8eaaa4b
DjRakso <DjRakso>
2020-11-05 00:06:43 -0500

    Added compatibility for 6/7/8 for RedHat and CentOS.
    - Added system version detection for various CentOS/Redhat systems.
    - Added commands to the script file.

--- install-openvpn.sh
+++ install-openvpn.sh
@@ -2,10 +2,10 @@
 #!/bin/sh
 ###############################################################################
 # Author:           DjRakso
-# Date:             November 4th, 2020
+# Date:             Thursday, November 5th, 2020
 # Description:      Install and configure OpenVPN for RedHat systems. Creates 
 #                   server and client configuration profiles.
-# Compatability:    CentOS / RedHat 6.0
+# Compatability:    RedHat / CentOS 6/7/8
 ###############################################################################
 # Copyright © by DjRakso 2020
 ###############################################################################
@@ -25,6 +25,36 @@ OPENVPN_SERVER_IP=10.0.8.0
 OPENVPN_SERVER_NETMASK=255.255.255.0
 OPENVPN_SERVER_SUBNET=24
 CIPHER=AES-256-CBC
+#####################
+
+
+################
+### COMMANDS ###
+################
+CONCATENATE_COMMAND=`which cat`
+CHANGE_DIRECTORY_COMMAND="cd"
+CHANGE_MODE_COMMAND="which chmod"
+EXECUTE_CHANGE_MODE_COMMAND="$CHANGE_MODE_COMMAND +x"
+CHECK_CONFIGURATION_SERVICES_COMMAND=`which chkconfig`
+COPY_COMMAND=`which cp`
+RECURSIVE_COPY_COMMAND="$COPY_COMMAND -r"
+ECHO_COMMAND=`which echo`
+EXIT_COMMAND="exit"
+FIREWALL_COMMAND=`firewall-cmd`
+IPTABLES_COMMAND=`which iptables`
+OPENSSL_COMMAND=`which openssl`
+OPENVPN_COMMAND=`which openvpn`
+REMOVE_COMMAND=`which rm`
+FORCE_REMOVE_COMMAND="$REMOVE_COMMAND -f"
+RECURSIVE_FORCE_COPY_COMMAND="$REMOVE_COMMAND -rf"
+SELINUX_POLICY_MANAGEMENT_COMMAND=`which semanage`
+STREAM_EDITOR_COMMAND=`which sed`
+SYSTEM_CONTROL_COMMAND=`which systemctl`
+TOUCH_COMMAND=`which touch`
+YUM_COMMAND=`which yum`
+YUM_INSTALL_COMMAND="$YUM_COMMAND -y install"
+YUM_UPDATE_COMMAND="$YUM_COMMAND -y update"
+################
 
 
 ###########################
@@ -39,10 +69,12 @@ DEV_NET_TUN_FILE=$DEV_NET_DIR/tun
 # /etc.
 ETC_DIR=/etc
 ETC_SYSTEM_CONFIGURATION_FILE=$ETC_DIR/sysctl.conf
+# /etc/init.d.
+ETC_INIT_D_DIR=$ETC_DIR/init.d
+OPENVPN_DAEMON=$ETC_INIT_D_DIR/openvpn
 # /etc/openvpn.
 ETC_OPENVPN_DIR=$ETC_DIR/openvpn
 ETC_OPENVPN_SERVER_CONF_FILE=$ETC_OPENVPN_DIR/server.conf
-
 # /etc/openvpn/easy-rsa.
 ETC_OPENVPN_EASY_RSA_DIR=$ETC_OPENVPN_DIR/easy-rsa
 ETC_OPENVPN_EASY_RSA_VARS_FILE=$ETC_OPENVPN_EASY_RSA_DIR/vars
@@ -50,6 +82,7 @@ ETC_OPENVPN_EASY_RSA_VARS_FILE=$ETC_OPENVPN_EASY_RSA_DIR/vars
 ETC_OPENVPN_EASY_RSA_VERSION_NAME=3.0.8
 ETC_OPENVPN_EASY_RSA_VERSION_DIR=$ETC_OPENVPN_EASY_RSA_DIR/$ETC_OPENVPN_EASY_RSA_VERSION_NAME
 OPENVPN_SERVER_DIFFIE_HELLMAN_PEM_FILE=$ETC_OPENVPN_EASY_RSA_VERSION_DIR/dh.pem
+OPENVPN_SERVER_EASYRSA_COMMAND=$ETC_OPENVPN_EASY_RSA_VERSION_DIR/easyrsa
 OPENVPN_SERVER_TLS_AUTHENTICATION_KEY_FILE=$ETC_OPENVPN_EASY_RSA_VERSION_DIR/tls-auth.key
 # /etc/openvpn/easy-rsa/<version>/pki.
 ETC_OPENVPN_EASY_RSA_VERSION_PKI_DIR=$ETC_OPENVPN_EASY_RSA_VERSION_DIR/pki
@@ -65,12 +98,19 @@ OPENVPN_SERVER_SERVER_KEY_FILE=$ETC_OPENVPN_EASY_RSA_VERSION_PKI_PRIVATE_DIR/ser
 # /etc/rc.d.
 ETC_RC_D_DIR=$ETC_DIR/rc.d
 ETC_RC_D_RC_LOCAL_FILE=$ETC_RC_D_DIR/rc.local
+# /proc.
+PROC_DIR=/proc
+PROC_SYS_DIR=$PROC_DIR/sys
+PROC_SYS_NET_DIR=$PROC_SYS_DIR/net
+PROC_SYS_NET_IPV4_DIR=$PROC_SYS_NET_DIR/ipv4
+IP_FORWARD_FILE=$PROC_SYS_NET_IPV4_DIR/ip_forward
 # /usr.
 USR_DIR=/usr
 # /usr/share.
 USR_SHARE_DIR=$USR_DIR/share
 # /usr/share/easy-rsa.
 USR_SHARE_EASY_RSA_DIR=$USR_SHARE_DIR/easy-rsa
+###########################
 
 
 ########################
@@ -78,148 +118,175 @@ USR_SHARE_EASY_RSA_DIR=$USR_SHARE_DIR/easy-rsa
 ########################
 INTERNAL_IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
 EXTERNAL_IP=$(curl ifconfig.me)
+OS_NAME=$(grep -oE '[a-Z]+' /etc/system-release | head -1)
+OS_VERSION=$(grep -oE '[0-9]+' /etc/system-release | head -1)
+########################
 
 
 #################
 ### FUNCTIONS ###
 #################
 # Update the system packages.
-yum -y update
+$YUM_UPDATE_COMMAND
 # Install the extra packages for enterprise linux. 
-yum -y install epel-release
+$YUM_INSTALL_COMMAND epel-release
 # Install the easy rsa, openvpn and policy core utilities python packages.
-yum -y install easy-rsa openvpn policycoreutils-python
+$YUM_INSTALL_COMMAND easy-rsa openvpn policycoreutils-python
 
 # Clean the /etc/opevpn/easy-rsa directory.
 if [ -d $ETC_OPENVPN_EASY_RSA_DIR ]; then
-	rm -rf $ETC_OPENVPN_EASY_RSA_DIR
+	$RECURSIVE_FORCE_COPY_COMMAND $ETC_OPENVPN_EASY_RSA_DIR
 fi
 
 # Copy the default Easy-RSA directory to the OpenVPN directory.
-cp -r $USR_SHARE_EASY_RSA_DIR $ETC_OPENVPN_EASY_RSA_DIR
+$RECURSIVE_COPY_COMMAND $USR_SHARE_EASY_RSA_DIR $ETC_OPENVPN_EASY_RSA_DIR
 
 # Set the RSA Key Size from the user settings.
-echo "set_var EASYRSA_KEY_SIZE $RSA_KEY_SIZE" > $ETC_OPENVPN_EASY_RSA_VARS_FILE
+$ECHO_COMMAND "set_var EASYRSA_KEY_SIZE $RSA_KEY_SIZE" > $ETC_OPENVPN_EASY_RSA_VARS_FILE
 
 # Change to the specific OpenVPN Easy RSA version.
-cd $ETC_OPENVPN_EASY_RSA_VERSION_DIR
+$CHANGE_DIRECTORY_COMMAND $ETC_OPENVPN_EASY_RSA_VERSION_DIR
 
 # Generate a Diffie-Hellman file.
-openssl dhparam $DH_KEY_SIZE -out $OPENVPN_SERVER_DIFFIE_HELLMAN_PEM_FILE
+$OPENSSL_COMMAND dhparam $DH_KEY_SIZE -out $OPENVPN_SERVER_DIFFIE_HELLMAN_PEM_FILE
 # Initialise your PKI.
-./easyrsa init-pki
+.$OPENVPN_SERVER_EASYRSA_COMMAND init-pki
 # Create your CA and disable password locking.
-./easyrsa --batch build-ca nopass
+.$OPENVPN_SERVER_EASYRSA_COMMAND --batch build-ca nopass
 # Build a server certificate & key and disable password locking.
-./easyrsa build-server-full server nopass
+.$OPENVPN_SERVER_EASYRSA_COMMAND build-server-full server nopass
 # Build a client certificate & key and disable password locking.
-./easyrsa build-client-full $CLIENT_NAME nopass
+.$OPENVPN_SERVER_EASYRSA_COMMAND build-client-full $CLIENT_NAME nopass
 # Create your revoke certificate.
-./easyrsa gen-crl
+.$OPENVPN_SERVER_EASYRSA_COMMAND gen-crl
 # Generate a TLS Authentication key.
-openvpn --genkey --secret $OPENVPN_SERVER_TLS_AUTHENTICATION_KEY_FILE
+$OPENVPN_COMMAND --genkey --secret $OPENVPN_SERVER_TLS_AUTHENTICATION_KEY_FILE
 
 # Copy all these files to the OpenVPN directory.
 ARRAY=($OPENVPN_SERVER_CA_CERTIFICATE_FILE $OPENVPN_SERVER_CA_KEY_FILE $OPENVPN_SERVER_DIFFIE_HELLMAN_PEM_FILE $OPENVPN_SERVER_SERVER_CERTIFICATE_FILE $OPENVPN_SERVER_SERVER_KEY_FILE $OPENVPN_SERVER_CRL_PEM_FILE)
 for FILE in "${ARRAY[@]}"; do
 	if [ -f $FILE ]; then
-		cp $FILE $ETC_OPENVPN_DIR/
+		$COPY_COMMAND $FILE $ETC_OPENVPN_DIR/
 		if [ $FILE == $OPENVPN_SERVER_CRL_PEM_FILE ]; then
-			chmod 644 $FILE
+			$CHANGE_MODE_COMMAND 644 $FILE
 		fi
 	else
-		exit 1
+		$EXIT_COMMAND 1
 	fi
 done
 
 # Remove the previous OpenVPN server configuration file.
-rm -f $ETC_OPENVPN_SERVER_CONF_FILE
+$FORCE_REMOVE_COMMAND $ETC_OPENVPN_SERVER_CONF_FILE
 # Create the OpenVPN server configuration file.
-echo "port $OPENVPN_PORT" >> $ETC_OPENVPN_SERVER_CONF_FILE
-echo "proto $OPENVPN_PROTOCOL" >> $ETC_OPENVPN_SERVER_CONF_FILE
-echo "dev tun" >> $ETC_OPENVPN_SERVER_CONF_FILE
-echo "user nobody" >> $ETC_OPENVPN_SERVER_CONF_FILE
-echo "group nogroup" >> $ETC_OPENVPN_SERVER_CONF_FILE
-echo "persist-key" >> $ETC_OPENVPN_SERVER_CONF_FILE
-echo "persist-tun" >> $ETC_OPENVPN_SERVER_CONF_FILE
-echo "keepalive 10 120" >> $ETC_OPENVPN_SERVER_CONF_FILE
-echo "topology subnet" >> $ETC_OPENVPN_SERVER_CONF_FILE
-echo "server $OPENVPN_SERVER_IP $OPENVPN_SERVER_NETMASK" >> $ETC_OPENVPN_SERVER_CONF_FILE
-echo "ifconfig-pool-persist ipp.txt" >> $ETC_OPENVPN_SERVER_CONF_FILE
-echo 'push "dhcp-option DNS $OPENVPNS_DNS"' >> $ETC_OPENVPN_SERVER_CONF_FILE
-echo 'push "redirect-gateway def1 bypass-dhcp"' >> $ETC_OPENVPN_SERVER_CONF_FILE
-echo "crl-verify crl.pem" >> $ETC_OPENVPN_SERVER_CONF_FILE
-echo "ca ca.crt" >> $ETC_OPENVPN_SERVER_CONF_FILE
-echo "cert server.crt" >> $ETC_OPENVPN_SERVER_CONF_FILE
-echo "key server.key" >> $ETC_OPENVPN_SERVER_CONF_FILE
-echo "tls-auth tls-auth.key 0" >> $ETC_OPENVPN_SERVER_CONF_FILE
-echo "dh dh.pem" >> $ETC_OPENVPN_SERVER_CONF_FILE
-echo "auth SHA512" >> $ETC_OPENVPN_SERVER_CONF_FILE
-echo "cipher $CIPHER" >> $ETC_OPENVPN_SERVER_CONF_FILE
-echo "tls-server" >> $ETC_OPENVPN_SERVER_CONF_FILE
-echo "tls-version-min 1.2" >> $ETC_OPENVPN_SERVER_CONF_FILE
-echo "tls-cipher TLS-DHE-RSA-WITH-AES-256-GCM-SHA384" >> $ETC_OPENVPN_SERVER_CONF_FILE
-echo "status openvpn.log" >> $ETC_OPENVPN_SERVER_CONF_FILE
-echo "verb 3" >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND "port $OPENVPN_PORT" >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND "proto $OPENVPN_PROTOCOL" >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND "dev tun" >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND "user nobody" >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND "group nogroup" >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND "persist-key" >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND "persist-tun" >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND "keepalive 10 120" >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND "topology subnet" >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND "server $OPENVPN_SERVER_IP $OPENVPN_SERVER_NETMASK" >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND "ifconfig-pool-persist ipp.txt" >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND 'push "dhcp-option DNS $OPENVPNS_DNS"' >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND 'push "redirect-gateway def1 bypass-dhcp"' >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND "crl-verify crl.pem" >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND "ca ca.crt" >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND "cert server.crt" >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND "key server.key" >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND "tls-auth tls-auth.key 0" >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND "dh dh.pem" >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND "auth SHA512" >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND "cipher $CIPHER" >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND "tls-server" >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND "tls-version-min 1.2" >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND "tls-cipher TLS-DHE-RSA-WITH-AES-256-GCM-SHA384" >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND "status openvpn.log" >> $ETC_OPENVPN_SERVER_CONF_FILE
+$ECHO_COMMAND "verb 3" >> $ETC_OPENVPN_SERVER_CONF_FILE
 
 # Touch the system configuration file if does not exist.
 if [ ! -f $ETC_SYSTEM_CONFIGURATION_FILE ]; then
-	touch $ETC_SYSTEM_CONFIGURATION_FILE
+	$TOUCH_COMMAND $ETC_SYSTEM_CONFIGURATION_FILE
 fi
 
 # Enable IP forwarding.
-sed -i '/\<net.ipv4.ip_forward\>/c\net.ipv4.ip_forward=1' $ETC_SYSTEM_CONFIGURATION_FILE
-echo 1 > /proc/sys/net/ipv4/ip_forward
+$STREAM_EDITOR_COMMAND -i '/\<net.ipv4.ip_forward\>/c\net.ipv4.ip_forward=1' $ETC_SYSTEM_CONFIGURATION_FILE
+$ECHO_COMMAND 1 > $IP_FORWARD_FILE
 
-# Set the NAT Post routing rules for the OpenVPN server.
-iptables -t nat -A POSTROUTING -s $OPENVPN_SERVER_IP/$OPENVPN_SERVER_SUBNET -j SNAT --to $INTERNAL_IP
-sed -i "1 a\iptables -t nat -A POSTROUTING -s $OPENVPN_SERVER_IP/$OPENVPN_SERVER_SUBNET -j SNAT --to $INTERNAL_IP" /etc/rc.d/rc.local
-
-# Set the Firewall rules for the OpenVPN server.
-iptables -I INPUT -p $OPENVPN_PROTOCOL --dport $OPENVPN_PORT -j ACCEPT
-iptables -I FORWARD -s $OPENVPN_SERVER_IP/$OPENVPN_SERVER_SUBNET -j ACCEPT
-iptables -I FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
-sed -i "1 a\iptables -I INPUT -p $OPENVPN_PROTOCOL --dport $OPENVPN_PORT -j ACCEPT" $ETC_RC_D_RC_LOCAL_FILE
-sed -i "1 a\iptables -I FORWARD -s $OPENVPN_SERVER_IP/$OPENVPN_SERVER_SUBNET -j ACCEPT" $ETC_RC_D_RC_LOCAL_FILE
-sed -i "1 a\iptables -I FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT" $ETC_RC_D_RC_LOCAL_FILE
+if [ "$OS_VERSION" -eq 6 ]; then
+	# Set the NAT Post routing rules for the OpenVPN server.
+	$IPTABLES_COMMAND -t nat -A POSTROUTING -s $OPENVPN_SERVER_IP/$OPENVPN_SERVER_SUBNET -j SNAT --to $INTERNAL_IP
+	$STREAM_EDITOR_COMMAND -i "1 a\iptables -t nat -A POSTROUTING -s $OPENVPN_SERVER_IP/$OPENVPN_SERVER_SUBNET -j SNAT --to $INTERNAL_IP" $ETC_RC_D_RC_LOCAL_FILE
+	
+	# Set the Firewall rules for the OpenVPN server.
+	$IPTABLES_COMMAND -I INPUT -p $OPENVPN_PROTOCOL --dport $OPENVPN_PORT -j ACCEPT
+	$IPTABLES_COMMAND -I FORWARD -s $OPENVPN_SERVER_IP/$OPENVPN_SERVER_SUBNET -j ACCEPT
+	$IPTABLES_COMMAND -I FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+	$STREAM_EDITOR_COMMAND -i "1 a\iptables -I INPUT -p $OPENVPN_PROTOCOL --dport $OPENVPN_PORT -j ACCEPT" $ETC_RC_D_RC_LOCAL_FILE
+	$STREAM_EDITOR_COMMAND -i "1 a\iptables -I FORWARD -s $OPENVPN_SERVER_IP/$OPENVPN_SERVER_SUBNET -j ACCEPT" $ETC_RC_D_RC_LOCAL_FILE
+	$STREAM_EDITOR_COMMAND -i "1 a\iptables -I FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT" $ETC_RC_D_RC_LOCAL_FILE
+elif [ "$OS_VERSION" -gt 6 ]; then
+	$FIREWALL_COMMAND --zone=public --add-port=$OPENVPN_PORT/$OPENVPN_PROTOCOL
+	$FIREWALL_COMMAND --permanent --zone=public --add-port=$OPENVPN_PORT/$OPENVPN_PROTOCOL
+	$FIREWALL_COMMAND --zone=trusted --add-source=$OPENVPN_SERVER_IP/$OPENVPN_SERVER_SUBNET
+	$FIREWALL_COMMAND --permanent --zone=trusted --add-source=$OPENVPN_SERVER_IP/$OPENVPN_SERVER_SUBNET
+else
+	$EXIT_COMMAND 2
+fi
 
 # Set the SELinux Policy Management tool for OpenVPN protocol and port.
-semanage port -a -t openvpn_port_t -p $OPENVPN_PROTOCOL $OPENVPN_PORT
+$SELINUX_POLICY_MANAGEMENT_COMMAND port -a -t openvpn_port_t -p $OPENVPN_PROTOCOL $OPENVPN_PORT
 
-# Restart the OpenVPN service.
-/etc/init.d/openvpn restart
-# Add OpenVPN service to the system services.
-chkconfig openvpn on
+if [ "$OS_VERSION" -eq 6 ]; then
+	# Restart the OpenVPN service.
+	$OPENVPN_DAEMON restart
+	# Add OpenVPN service to the system services.
+	$CHECK_CONFIGURATION_SERVICES_COMMAND openvpn on
+elif [ "$OS_VERSION" -gt 6 ]; then
+	# Restart the OpenVPN service.
+	$SYSTEM_CONTROL_COMMAND restart openvpn@server.service
+	# Add OpenVPN service to the system services.
+	$SYSTEM_CONTROL_COMMAND enable openvpn@server.service
+else
+	$EXIT_COMMAND 3
+fi
 
 # Remove the previous OpenVPN server configuration file.
-rm -f $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$FORCE_REMOVE_COMMAND $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
 # Create the OpenVPN client configuration file.
-echo "client" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-echo "proto $OPENVPN_PROTOCOL" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-echo "remote $EXTERNAL_IP $OPENVPN_PORT" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-echo "dev tun" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-echo "resolv-retry infinite" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-echo "nobind" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-echo "persist-key" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-echo "persist-tun" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-echo "remote-cert-tls server" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-echo "auth SHA512" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-echo "cipher $CIPHER" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-echo "tls-client" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-echo "tls-version-min 1.2" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-echo "tls-cipher TLS-DHE-RSA-WITH-AES-256-GCM-SHA512" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-echo "setenv opt block-outside-dns" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-echo "verb 3" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-echo "<ca>" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-cat $OPENVPN_SERVER_CA_CERTIFICATE_FILE >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-echo "</ca>" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-echo "<cert>" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-cat $ETC_OPENVPN_EASY_RSA_VERSION_PKI_ISSUED_DIR/$CLIENT_NAME.crt >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-echo "</cert>" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-echo "<key>" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-cat $ETC_OPENVPN_EASY_RSA_VERSION_PKI_PRIVATE_DIR/$CLIENT_NAME.key >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-echo "</key>" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-echo "key-direction 1" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-echo "<tls-auth>" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-cat $ETC_OPENVPN_DIR/tls-auth.key >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
-echo "</tls-auth>" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$ECHO_COMMAND "client" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+if [ "$OPENVPN_PROTOCOL" == "tcp" ]; then
+	$ECHO_COMMAND "proto tcp-client"
+else
+	$ECHO_COMMAND "proto $OPENVPN_PROTOCOL" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+fi
+$ECHO_COMMAND "remote $EXTERNAL_IP $OPENVPN_PORT" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$ECHO_COMMAND "dev tun" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$ECHO_COMMAND "resolv-retry infinite" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$ECHO_COMMAND "nobind" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$ECHO_COMMAND "persist-key" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$ECHO_COMMAND "persist-tun" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$ECHO_COMMAND "remote-cert-tls server" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$ECHO_COMMAND "auth SHA512" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$ECHO_COMMAND "cipher $CIPHER" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$ECHO_COMMAND "tls-client" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$ECHO_COMMAND "tls-version-min 1.2" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$ECHO_COMMAND "tls-cipher TLS-DHE-RSA-WITH-AES-256-GCM-SHA512" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$ECHO_COMMAND "setenv opt block-outside-dns" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$ECHO_COMMAND "verb 3" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$ECHO_COMMAND "<ca>" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$CONCATENATE_COMMAND $OPENVPN_SERVER_CA_CERTIFICATE_FILE >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$ECHO_COMMAND "</ca>" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$ECHO_COMMAND "<cert>" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$CONCATENATE_COMMAND $ETC_OPENVPN_EASY_RSA_VERSION_PKI_ISSUED_DIR/$CLIENT_NAME.crt >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$ECHO_COMMAND "</cert>" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$ECHO_COMMAND "<key>" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$CONCATENATE_COMMAND $ETC_OPENVPN_EASY_RSA_VERSION_PKI_PRIVATE_DIR/$CLIENT_NAME.key >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$ECHO_COMMAND "</key>" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$ECHO_COMMAND "key-direction 1" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$ECHO_COMMAND "<tls-auth>" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$CONCATENATE_COMMAND $OPENVPN_SERVER_TLS_AUTHENTICATION_KEY_FILE >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+$ECHO_COMMAND "</tls-auth>" >> $ETC_OPENVPN_DIR/$CLIENT_NAME.ovpn
+#################
+
